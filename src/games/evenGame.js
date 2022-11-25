125dc04e1ddb219ef7c8f2aaf809e298262e0dd8
stepan korshukov <korshukovstepan@gmail.com>
2022-11-26 01:25:48 +0300

    fix: make a refactoring of brain-even game file to prepare it import to game engine

--- src/games/evenGame.js
+++ src/games/evenGame.js
@@ -1,35 +1,17 @@
-import gamesLogic from '../index.js';
+const description =
+  'Answer "yes" if the number is even, otherwise answer "no".';
 
-const brainEvenGame = () => {
-  const randomNumber = Math.floor(Math.random() * 20);
-  const rightAnswer = randomNumber % 2 === 0 ? 'yes' : 'no';
-  gamesLogic(
-    'Answer "yes" if the number is even, otherwise answer "no".',
-    `Question: ${randomNumber}`,
-    rightAnswer,
-  );
+const getRandomNum = (min, max) =>
+  Math.round(Math.random() * (max - min)) + min;
+
+const isEven = (num) => num % 2 === 0;
+const evenRound = () => {
+  const question = getRandomNum(1, 10);
+  const correctAnswer = isEven(question) ? 'yes' : 'no';
+
+  return [question, correctAnswer];
 };
 
-export default brainEvenGame;
+const evenGame = () => runGame(description, evenRound);
 
-// gamesLogic.greeting();
-// const userName = gamesLogic.getUserName()
-// gamesLogic.taskDescription(
-//   'Answer "yes" if the number is even, otherwise answer "no".'
-// );
-
-// export const brainEvenGame = () => {
-
-//   gamesLogic.nextStep(userName, userAnswer, rightAnswer);
-// };
-
-  // const userName = gamesLogic.getUserName();
-  // const randomNumber = Math.floor(Math.random() * 20);
-  // gamesLogic.taskDescription(
-  //   'Answer "yes" if the number is even, otherwise answer "no".'
-  // );
-  // gamesLogic.askQuestion(`Question: ${randomNumber}`);
-  // const rightAnswer = randomNumber % 2 === 0 ? 'yes' : 'no';
-  // const userAnswer = gamesLogic.getAnswerFortheQuestion('Your answer: ');
-
-  // gamesLogic.nextStep(userAnswer, rightAnswer, userName);
+export default evenGame;
