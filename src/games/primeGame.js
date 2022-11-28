cca16faf6142d26adde57797784cb42e9bf0edd4
stepan korshukov <korshukovstepan@gmail.com>
2022-11-29 01:54:25 +0300

    fix: try to fix mistakes in my code

--- src/games/primeGame.js
+++ src/games/primeGame.js
@@ -1,25 +1,23 @@
 import { getRandomNum } from '../cli.js';
 import runGame from '../index.js';
 
-const description =
-  'Answer "yes" if given number is prime. Otherwise answer "no".';
-
-const randomNumber = getRandomNum(1, 100);
-
-const isPrime = (randomNumber) => {
-    if (randomNumber < 1) {
-        return 'no';
-    } else if ((randomNumber % randomNumber !== 0) && (randomNumber % 1 !== 0)) {
-        return 'yes';
-    } else {
-        return 'no';
-    }
-};
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const primeRound = () => {
-  const question = randomNumber;
-  const correctAnswer = isPrime();
+  const randomNumber = getRandomNum(1, 100);
+  let correctAnswer = '';
 
+  if (
+    (randomNumber > 1) 
+    || (randomNumber % randomNumber !== 0) 
+    && (randomNumber % 1 !== 0)
+  ) {
+    correctAnswer = 'yes';
+  } else {
+    correctAnswer = 'no';
+  }
+
+  const question = randomNumber;
   return [question, correctAnswer.toString()];
 };
 
