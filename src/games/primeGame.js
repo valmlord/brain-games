ef37ccc6655c76a261e564afae8a016827e05005
stepan korshukov <korshukovstepan@gmail.com>
2022-11-29 12:17:55 +0300

    fix: make refactoring in brain-prime game, add new links to asciinemas for all games in README.md

--- src/games/primeGame.js
+++ src/games/primeGame.js
@@ -3,22 +3,20 @@ import runGame from '../index.js';
 
 const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
+const isPrime = (num) => {
+  for (let i = 2, max = Math.sqrt(num); i <= max; i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return num > 1;
+};
+
 const primeRound = () => {
   const randomNumber = getRandomNum(1, 100);
-  let correctAnswer = '';
-
-  if (
-    (randomNumber > 1) 
-    || (randomNumber % randomNumber !== 0) 
-    && (randomNumber % 1 !== 0)
-  ) {
-    correctAnswer = 'yes';
-  } else {
-    correctAnswer = 'no';
-  }
-
+  const correctAnswer = isPrime(randomNumber) ? 'yes' : 'no';
   const question = randomNumber;
-  return [question, correctAnswer.toString()];
+  return [question, correctAnswer];
 };
 
 const primeGame = () => runGame(description, primeRound);
