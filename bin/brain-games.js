d2700990812dcd6d10c6c19bc44a6e556c169f6a
stepan korshukov <korshukovstepan@gmail.com>
2022-11-29 12:47:52 +0300

    fix: import function getName in brain-games.js to avoid warnings in codeclimate

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,10 +1,4 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
+import { getName } from '../src/cli.js'
 
-const getName = () => {
-  console.log('Welcome to the Brain Games!');
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-};
-
-export default getName;
+getName();
