b898f23986fed5d2551d229573ff1cc280891d4d
stepan korshukov <korshukovstepan@gmail.com>
2022-11-29 12:42:17 +0300

    fix: resolve all mistakes in linter and make little refactoring in brain-gcd game

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
 
-export const getName = () => {
+const getName = () => {
   console.log('Welcome to the Brain Games!');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
 };
 
-getName();
+export default getName;
--- src/games/gcdGame.js
+++ src/games/gcdGame.js
@@ -3,23 +3,14 @@ import runGame from '../index.js';
 
 const description = 'Find the greatest common divisor of given numbers.';
 
+const getGcd = (num1, num2) => (!num2 ? num1 : getGcd(num2, num1 % num2));
+
 const gcdRound = () => {
   const firstOperand = getRandomNum(1, 100);
   const secondOperand = getRandomNum(1, 100);
   const question = `${firstOperand} ${secondOperand}`;
 
-  function getGcd(a, b) {
-    while (a !== 0 && b !== 0) {
-      if (a > b) {
-        a %= b;
-      } else {
-        b %= a;
-      }
-    }
-    return a + b;
-  }
-
-  const correctAnswer = getGcd(firstOperand, secondOperand);
+  const correctAnswer = `${getGcd(firstOperand, secondOperand)}`;
 
   return [question, correctAnswer.toString()];
 };
